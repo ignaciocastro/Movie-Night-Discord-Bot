88f60de35f91a7efe0e84722889462c1d1056599
Ignacio Castro <ignaciocastrom@gmail.com>
2020-10-03 19:58:49 -0300

    addMoviesRole.js - Spanish translation

--- commands/addMoviesRole.js
+++ commands/addMoviesRole.js
@@ -1,16 +1,16 @@
 module.exports = {
 	name: "moviesrole",
-	description: "Sets a role that is allowed to add movies to the servers list. Can also clear this role by using moviesrole remove",
+	description: "Establece un rol que puede agregar películas a la lista del servidor. Tambien puedes borrar este rol usando moviesrole remove",
 	usage: "[@roleName]",
 	args: true,
 	admin: true,
 	async execute(message, args, main, callback) {
 		if (args.length > 1) {
-			message.channel.send("Please only specify one mentioned role.");
+			message.channel.send("Por favor especifica solo un rol.");
 
 			return callback();
 		} else if (!message.mentions.roles.first() && args[0] != "clear") {
-			message.channel.send("Please mention the role you'd like to set in the format moviesrole [@roleName], or to clear settings use moviesrole clear");
+			message.channel.send("Por favor menciona el rol que quieres seleccionar en el formato moviesrole [@nombreRol], o para limpiar los ajustes usa moviesrole clear");
 
 			return callback();
 		} else {
@@ -19,9 +19,9 @@ module.exports = {
 			//Update the settings with the role user provided, or clear it and set to NULL.
 			return main.setting.updateOne({ guildID: message.guild.id }, { "addMoviesRole": addMoviesRole }, function(err) {
 				if (!err) {
-					message.channel.send(addMoviesRole ? `Users with administrator or the role ${args[0]} will now be able to add movies.` : "Setting for role allowed to add movies has been cleared. Anyone will be able to add movies now.");
+					message.channel.send(addMoviesRole ? `Usuarios administradores o con el rol ${args[0]} ahora podrán añadir películas.` : "Ajustes para el rol limpios. Ahora todos podrán añadir películas.");
 				} else {
-					message.channel.send("Couldn't set role for adding permissions, something went wrong");
+					message.channel.send("No se pudo establecer el rol para añadir permisos, ocurrió un error.");
 				}
 
 				return callback();
