7017faae550208336f8490b84dbe8bf783dffb6f
Ignacio Castro <ignaciocastrom@gmail.com>
2020-10-03 20:53:32 -0300

    autoView.js - Spanish translation

--- commands/autoView.js
+++ commands/autoView.js
@@ -1,11 +1,11 @@
 module.exports = {
 	name: "autoview",
-	description: "Turns on or off auto view after poll is complete (Hides movie from future polls).",
-	usage: "[on or off]",
+	description: "Enciende o apaga auto ver después de que termine la encuesta (Esconde la película de encuestas futuras).",
+	usage: "[on u off]",
 	admin: true,
 	async execute(message, args, main, callback, settings) {
 		if ((args.length > 1 || !args.length) || (args[0].toLowerCase() != "on" && args[0].toLowerCase() != "off")) {
-			message.channel.send(`Please only specify on or off. Currently setting is: ${settings.autoViewed ? 'on' : 'off'}`);
+			message.channel.send(`Por favor solo especifica on u off. El ajuste actual es: ${settings.autoViewed ? 'on' : 'off'}`);
 
 			return callback();
 		} else {
@@ -13,9 +13,9 @@ module.exports = {
 
 			return main.setting.updateOne({ guildID: message.guild.id }, { "autoViewed": autoView }, function(err) {
 				if (!err) {
-					message.channel.send(`Auto view has now been set to: ${autoView ? "On" : "Off"}`);
+					message.channel.send(`Auto view ahora está: ${autoView ? "On" : "Off"}`);
 				} else {
-					message.channel.send("Couldn't set auto view, something went wrong");
+					message.channel.send("No se pudo activar auto view, ocurrió un problema");
 				}
 
 				return callback();
