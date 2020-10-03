75449d951af335962f88d101c3d7818b1c562824
Ignacio Castro <ignaciocastrom@gmail.com>
2020-10-03 20:12:06 -0300

    New translations setViewed.js (Spanish, Chile)

---
+++ commands/es/setViewed.js
@@ -0,0 +1,29 @@
+module.exports = {
+    name: 'setviewed',
+    description: 'Toggles specified movie as viewed.',
+    usage: '[Movie name to set as viewed]',
+    args: true,
+    admin: true,
+    async execute(message, args, main, callback) {
+        var movie = args.join(' ');
+        var searchOptions = main.searchMovieDatabaseObject(message.guild.id, movie);
+        return main.movieModel.findOne(searchOptions, function (err, movie) {
+            if (err || !movie) {
+                message.channel.send('Movie could not be found!');
+                return callback();
+            } else {
+                movie.updateOne({
+                    viewed: !movie.viewed,
+                    viewedDate: movie.viewed ? null : new Date()
+                }, function (err) {
+                    if (!err) {
+                        message.channel.send(`${ movie.name } has been set to ${ !movie.viewed ? 'viewed' : 'unviewed' }!`);
+                    } else {
+                        message.channel.send('Could not set movie to viewed, something went wrong.');
+                    }
+                    return callback();
+                });
+            }
+        });
+    }
+};
