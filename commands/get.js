07626806cc45cf756113310e8c683182500fb9d1
Ignacio Castro <ignaciocastrom@gmail.com>
2020-10-03 20:58:42 -0300

    get.js - Spanish translation

--- commands/get.js
+++ commands/get.js
@@ -3,7 +3,7 @@ const moment = require("moment");
 
 module.exports = {
 	name: "get",
-	description: "Returns list of all movies in current watch list for server, or if search is specified it will attempt to search the servers list for the movie.",
+	description: "Entrega una lista de todas las películas en la watch list del server, o, si search es especificado intentará buscar la lista del servidor para la película.",
 	aliases: ["list", "getmovie"],
 	execute(message, args, main, callback) {
 		var embeddedMessages = [];
@@ -17,26 +17,26 @@ module.exports = {
 			//return to avoid hitting logic below.
 			return main.movieModel.find(searchOptions, function (error, movies) {
 				if (error) {
-					message.channel.send("Could not return list of movies, an error occured.");
+					message.channel.send("No se pudo mostrar la lista de películas, ocurrió un problema.");
 
 					return callback();
 				}
 				
 				if (movies.length == 0) { 
-					message.channel.send("List of unviewed movies is currently empty.");
+					message.channel.send("La lista de películas aun no vistas está vacía.");
 
 					return callback();
 				} else if (movies && movies.length > 0) {
 					for (var movie of movies) {
-						var stringConcat = `**[${number}. ${movie.name}](https://www.imdb.com/title/${movie.imdbID})** submitted by ${movie.submittedBy} on ${moment(movie.submitted).format("DD MMM YYYY")}\n
-						**Release Date:** ${moment(movie.releaseDate).format("DD MMM YYYY")} **Runtime:** ${movie.runtime} Minutes **Rating:** ${movie.rating}\n\n`;
+						var stringConcat = `**[${number}. ${movie.name}](https://www.imdb.com/title/${movie.imdbID})** solicitada por ${movie.submittedBy} en ${moment(movie.submitted).format("DD MMM YYYY")}\n
+						**Fecha de estreno:** ${moment(movie.releaseDate).format("DD MMM YYYY")} **Duración:** ${movie.runtime} Minutos **Rating:** ${movie.rating}\n\n`;
 
 						//If the length of message has become longer than DISCORD API max, we split the message into a seperate embedded message.
 						if (description.length + stringConcat.length > 2048) {
 							movieEmbed.setDescription(description);
 							embeddedMessages.push(movieEmbed);
 							description = "";
-							movieEmbed = new MessageEmbed().setTitle("Submitted Movies (Cont...)").setColor("#6441a3");
+							movieEmbed = new MessageEmbed().setTitle("Películas solicitadas (Cont...)").setColor("#6441a3");
 						} 
 
 						description += stringConcat;
@@ -62,7 +62,7 @@ module.exports = {
 			if (movie) {
 				message.channel.send(main.buildSingleMovieEmbed(movie));		
 			} else {
-				message.channel.send("Could not find movie in your list. Perhaps try using the search command instead?");
+				message.channel.send("No se encontró la película en tu lista. ¿Quizas intenta usando el comando de busqueda?");
 			}
 
 			return callback();
