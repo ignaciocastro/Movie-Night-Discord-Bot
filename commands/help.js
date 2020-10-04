a726067357b71b61660a0bdb6400f7d9bd6135fd
Ignacio Castro <ignaciocastrom@gmail.com>
2020-10-03 21:04:07 -0300

    help.js - Spanish translation

--- commands/help.js
+++ commands/help.js
@@ -2,25 +2,25 @@ const { prefix } = require('../config.json');
 
 module.exports = {
 	name: "help",
-	description: "List all of my commands or info about a specific command.",
+	description: "Muestra todos los comandos o información sobre un comando específico",
 	aliases: ["commands"],
-	usage: "[command name]",
+	usage: "[nombre de comando]",
 	execute(message, args) {
 		const data = [];
 		const { commands } = message.client;
 
 		if (!args.length) {
-			data.push("Here's a list of all my commands:");
+			data.push("Aquí hay una lista de todos mis comandos:");
 			data.push(commands.map(command => command.name).join(", "));
-			data.push(`\nYou can send \`${prefix}help [command name]\` to get info on a specific command!`);
+			data.push(`\n¡Puedes usar \`${prefix}help [nombre comando]\` para obtener información de un comando en específico!`);
 
 			return message.author.send(data, { split: true }).then(() => {
 				if (message.channel.type === "dm") return;
-				return message.reply("I've sent you a DM with all my commands!");
+				return message.reply("Te he enviado un MD con todos mis comandos.");
 			}).catch(error => {
-				console.error(`Could not send help DM to ${message.author.tag}.\n`, error);
+				console.error(`No se pudo enviar MD a ${message.author.tag}.\n`, error);
 				
-				return message.reply("it seems like I can't DM you! Check your discord privacy settings to allow server members to DM you.");
+				return message.reply("¡Pareciera que no te puedo mandar MD! Revisa tus ajustes de privacidad de Discord para permitir mensajes de miembros del servidor.");
 			});
 		}
 
@@ -28,14 +28,14 @@ module.exports = {
 		const command = commands.get(name) || commands.find(c => c.aliases && c.aliases.includes(name));
 
 		if (!command) {
-			return message.reply("That's not a valid command!");
+			return message.reply("¡Ese no es un comando válido!");
 		}
 
-		data.push(`**Name:** ${command.name}`);
+		data.push(`**Nombre:** ${command.name}`);
 
 		if (command.aliases) data.push(`**Aliases:** ${command.aliases.join(", ")}`);
-		if (command.description) data.push(`**Description:** ${command.description}`);
-		if (command.usage) data.push(`**Usage:** ${prefix}${command.name} ${command.usage}`);
+		if (command.description) data.push(`**Descripción:** ${command.description}`);
+		if (command.usage) data.push(`**Uso:** ${prefix}${command.name} ${command.usage}`);
 
 		return message.channel.send(data, { split: true });
 	},
